76bdaa92a1bf5311be87c99dae4e8bb650178235
gaurav-15 <44693687+gaurav-15@users.noreply.github.com>
2019-03-18 21:46:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
+app.use(express.static(__dirname + '/pages'));
 app.get("/", function (req, res) {
-    res.redirect("https://iiitd-pathways.firebaseapp.com/");
+    res.redirect('/index.html');
+    res.end();
 });
 
 app.post("/", function (req, res) {
-    res.redirect("https://iiitd-pathways.firebaseapp.com/");
+    res.redirect('/index.html');
+    res.end();
 });
 
 
 
 app.listen(8080, function () {
-   console.log("listening on port: 8080");
+    console.log("listening on port: 8080");
 });
