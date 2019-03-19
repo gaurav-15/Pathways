1399a06ad105d389a377a42f16ab14948b78d1f9
gaurav-15 <44693687+gaurav-15@users.noreply.github.com>
2019-03-19 12:42:38 +0530

    Add files via upload

---
+++ connect.js
@@ -0,0 +1,10 @@
+module.exports={
+    connectDB: function (result) {
+        console.log("attempting DB connection");
+        var mongo=require('mongodb').MongoClient;
+        var uri="mongodb+srv://admin:admin@pathways-t30da.mongodb.net/test?retryWrites=true";
+        mongo.connect(uri, { useNewUrlParser: true }, function (mongoError, mongoClient) {
+            return result(mongoError, mongoClient);
+        });
+    }
+};
---
+++ index.js
@@ -0,0 +1,24 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+var app=express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function (req, res) {
+    console.log("GET request");
+    var connect=require('./connect');
+    connect.connectDB(function (err, client) {
+        if (err) {
+            console.error(err);
+        }
+        console.log("connected to db");
+    });
+    res.end();
+});
+
+app.listen(8080, function () {
+    console.log("Listening on port: 8080");
+});
