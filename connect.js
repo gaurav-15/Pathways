a985f28da11dfa3407c9776e441bf641c427735e
gaurav-15 <44693687+gaurav-15@users.noreply.github.com>
2019-03-20 16:26:07 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,10 +1,10 @@
 module.exports={
-    connectDB: function (result) {
+    connectDB: function (callback) {
         console.log("attempting DB connection");
         var mongo=require('mongodb').MongoClient;
         var uri="mongodb+srv://admin:admin@pathways-t30da.mongodb.net/Pathways_db?retryWrites=true";
         mongo.connect(uri, { useNewUrlParser: true }, function (mongoError, mongoClient) {
-            return result(mongoError, mongoClient);
+            return callback(mongoError, mongoClient);
         });
     }
 };
