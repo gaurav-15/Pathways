dafe00c78ddb139b4c8ccec00cef658adeeb159e
gaurav-15 <44693687+gaurav-15@users.noreply.github.com>
2019-03-20 14:07:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,35 @@ app.get('/',function (req, res) {
     res.end();
 });
 
+app.post('/', function (req, res) {
+    console.log("POST from /");
+    res.redirect('https://pathways-iiitdelhi.firebaseapp.com/');
+    res.end();
+});
+
+
+app.post('/getCourses', function (req, res) {
+    var connect=require('./connect');
+    connect.connectDB(function (err, client) {
+        if (err) {
+            console.error(err);
+            throw err;
+        }
+        var courses=client.db('Pathways_db').collection('Courses').find({});
+        courses.toArray(function (mongoError, objects) {
+            if (err) {
+                console.error(err);
+                throw err;
+            }
+            //console.log(objects);
+            res.write(JSON.stringify(objects));
+            client.close();
+            res.end();
+            console.log("Connection closed");
+        });
+    });
+});
+
 app.listen(8080, function () {
     console.log("Listening on port: 8080");
 });
