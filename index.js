9c7e099d223bce1dbf37f57a0649afd68dbab6e6
gaurav-15 <44693687+gaurav-15@users.noreply.github.com>
2019-04-25 10:26:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var express=require('express');
-var bodyParser=require('body-parser');
-var app=express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -29,8 +29,8 @@ app.post('/courses', function (req, res) {
 
 
 app.post('/login', function (req, res) {
-    var name_i=req.body.name;
-    var email_i=req.body.email;
+    let name_i=req.body.name;
+    let email_i=req.body.email;
     console.log("name: "+name_i);
     console.log("email: "+email_i);
     login(email_i, name_i, function (response) {
@@ -43,13 +43,13 @@ app.post('/login', function (req, res) {
 
 
 app.post('/addCourse', function (req, res) {
-    var name_i=req.body.title.trim();
-    var code_i=req.body.code.toUpperCase().trim();
-    var semester_i=req.body.semester.trim();
-    var credits_i=req.body.credits.trim();
-    var prerequisites_i=req.body.prerequisites;
-    var antirequisites_i=req.body.antirequisites;
-    var tags_i=req.body.tags;
+    let name_i=req.body.title.trim();
+    let code_i=req.body.code.toUpperCase().trim();
+    let semester_i=req.body.semester.trim();
+    let credits_i=req.body.credits.trim();
+    let prerequisites_i=req.body.prerequisites;
+    let antirequisites_i=req.body.antirequisites;
+    let tags_i=req.body.tags;
     addCourse(name_i,code_i,semester_i,credits_i,prerequisites_i,antirequisites_i, tags_i,function (response) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -59,7 +59,7 @@ app.post('/addCourse', function (req, res) {
 
 
 app.post("/searchCourses", function (req, res) {
-    var name=req.body.search.toLowerCase().trim();
+    let name=req.body.search.toLowerCase().trim();
     searchCourses(name,function (response) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -67,6 +67,50 @@ app.post("/searchCourses", function (req, res) {
     });
 });
 
+app.post("/searchCoursesbyid", function (req, res) {
+    let name=req.body.code.toLowerCase().trim();
+    //console.log(name);
+    searchCoursesbyid(name,function (response) {
+        getDependencies(response,function (response) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.end(response);
+        });
+    });
+});
+
+
+
+async function getDependencies(json_str,callback) {
+    const async = require('async');
+    let json = JSON.parse(json_str);
+    let queue = [];
+    let visited = [];
+    let result={};
+    result[json[0].code]=[];
+    for (let i=json[0].prerequisites.length-1;i>=0;i--) {
+        result[json[0].code].push(json[0].prerequisites[i]);
+        queue.push(json[0].prerequisites[i]);
+    }
+    while (queue.length!==0){
+        let cid = queue.pop();
+        result[cid]=[];
+        visited.push(cid);
+        await new Promise(function (callback) {
+            searchCoursesbyid(cid,function (response) {
+                json=JSON.parse(response);
+                for (let i=json[0].prerequisites.length-1;i>=0;i--) {
+                    if (visited.indexOf(json[0].prerequisites[i])===-1) {
+                        result[cid].push(json[0].prerequisites[i]);
+                        queue.push(json[0].prerequisites[i]);
+                    }
+                }
+                callback();
+            });
+        });
+    }
+    callback(JSON.stringify(result));
+}
 
 function searchCourses(searchKey, callback) {
     connectDB(function (err, client)  {
@@ -75,8 +119,8 @@ function searchCourses(searchKey, callback) {
             throw err;
         }
         client.db("Pathways_db").collection('Courses').find({$or:[{"tags":{$regex: new RegExp(searchKey, "i")}},{"name":{$regex: new RegExp(searchKey, "i")}}]}).toArray(function (mongoError, objects) {
-            var response=JSON.stringify(objects);
-            console.log(response);
+            let response=JSON.stringify(objects);
+            //console.log(response);
             client.close();
             callback(response);
         });
@@ -84,13 +128,19 @@ function searchCourses(searchKey, callback) {
 }
 
 
-function getDependencies(response, callback) {
-    var courses=JSON.parse(response);
-    for (var i=0;i<courses.size();i++) {
-        var d=true;
-        while (d) {
+
+function searchCoursesbyid(searchKey, callback) {
+    connectDB(function (err, client)  {
+        if (err) {
+            console.error(err);
+            throw err;
         }
-    }
+        client.db("Pathways_db").collection('Courses').find({"code":{$regex: new RegExp(searchKey, "i")}}).toArray(function (mongoError, objects) {
+            let response=JSON.stringify(objects);
+            client.close();
+            callback(response);
+        });
+    })
 }
 
 
@@ -111,8 +161,8 @@ function addCourse(name_i, code_i, semester_i, credits_i, prerequisites_i, antir
                     if (!result) {
                         callback("0");
                     } else {
-                        var prereq=JSON.parse(prerequisites_i);
-                        for (var i=0;i<prereq.length;i++) {
+                        let prereq=JSON.parse(prerequisites_i);
+                        for (let i=0;i<prereq.length;i++) {
                             client.db('Pathways_db').collection('Courses').updateOne({code: new RegExp('^'+prereq[i]+'$', "i")}, {$push: {options:code_i}}, function (err, upd) {
                                 if (err) {
                                     console.error(err);
@@ -197,8 +247,8 @@ function getCourses(callback) {
             console.log(err);
             throw err;
         }
-        var courses=client.db('Pathways_db').collection('Courses').find({});
-        var response="";
+        let courses=client.db('Pathways_db').collection('Courses').find({});
+        let response="";
         courses.toArray(function (mongoError, objects) {
             response=JSON.stringify(objects);
             console.log(response);
@@ -210,8 +260,8 @@ function getCourses(callback) {
 
 
 function connectDB(callback) {
-    var mongo=require('mongodb').MongoClient;
-    var uri="mongodb+srv://admin:admin@pathways-t30da.mongodb.net/Pathways_db?retryWrites=true";
+    let mongo=require('mongodb').MongoClient;
+    let uri="mongodb+srv://admin:admin@pathways-t30da.mongodb.net/Pathways_db?retryWrites=true";
     mongo.connect(uri, { useNewUrlParser: true }, function (mongoError, mongoClient) {
         return callback(mongoError, mongoClient);
     });
